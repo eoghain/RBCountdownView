2ab44c8990b0256942f74a8ff6d1797674e41805
Robert Booth <Robert_Booth@intuit.com>
2015-11-05 16:56:32 -0800

    Initial Commit

---
+++ countdown/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  countdown
+//
+//  Created by Booth, Robert on 11/5/15.
+//  Copyright © 2015 Booth, Robert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ countdown/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  countdown
+//
+//  Created by Booth, Robert on 11/5/15.
+//  Copyright © 2015 Booth, Robert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
